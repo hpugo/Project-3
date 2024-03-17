148f69b5d819a31bd12b86eaaa9c5f9192cd1a3e
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-17 10:21:03 -0500

    Adding some readme, also starting to add the routes to the website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Project-3
+# Henry's Soccer League!
+
+This website allows you to look at your soccer league's progress. From the league standing of your team, to your teams next match!
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.use('/teams', require('./controllers/teams'))
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 app.listen(process.env.PORT)
 
